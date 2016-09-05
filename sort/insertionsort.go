fb6b544ca8a7d7a247e6e001a64fd009aed8e6a3
Jean de Klerk <jadekler@gmail.com>
2016-09-05 11:10:19 -0600

    Finish insertionsort

--- sort/insertionsort.go
+++ sort/insertionsort.go
@@ -1,5 +1,25 @@
 package sort
 
 func Insertionsort(arr []int) []int {
-    return []int{}
+    for i := 0; i < len(arr); i++ {
+        var j int
+        elem := arr[i]
+
+        for j = i; j > 0 && elem < arr[j - 1]; j-- {
+
+        }
+
+        arr = shiftup(arr, j, i)
+        arr[j] = elem
+    }
+
+    return arr
+}
+
+func shiftup(arr []int, start, end int) []int {
+    for i := end; i > start; i-- {
+        arr[i] = arr[i - 1]
+    }
+
+    return arr
 }
