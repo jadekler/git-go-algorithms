2f6054d20d786d1740ed1264fc40960d48389b0b
Jean de Klerk <jadekler@gmail.com>
2016-09-05 10:31:10 -0600

    Added more tests for sorts

--- sort/mergesort_test.go
+++ sort/mergesort_test.go
@@ -14,3 +14,21 @@ func TestMergesort(t *testing.T) {
         t.Fail()
     }
 }
+
+func TestMergesortSame(t *testing.T) {
+    actual := sort.Mergesort([]int{1, 2, 3})
+    expected := []int{1, 2, 3}
+    if !reflect.DeepEqual(actual, expected) {
+        t.Logf("Expected %v, got %v", expected, actual)
+        t.Fail()
+    }
+}
+
+func TestMergesortComplex(t *testing.T) {
+    actual := sort.Mergesort([]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1})
+    expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    if !reflect.DeepEqual(actual, expected) {
+        t.Logf("Expected %v, got %v", expected, actual)
+        t.Fail()
+    }
+}
