d082dd3bb9da40bbfcdb3e172a18aaee2f1cab69
Jean de Klerk <jadekler@gmail.com>
2016-09-05 11:53:46 -0600

    Finished mergesort

--- sort/mergesort.go
+++ sort/mergesort.go
@@ -1,5 +1,54 @@
 package sort
 
+import "fmt"
+
 func Mergesort(arr []int) []int {
-    return []int{}
+    return mergesortActual(arr, pivotPointFn(arr))
+}
+
+func pivotPointFn(arr []int) int {
+    return len(arr) / 2
+}
+
+func mergesortActual(arr []int, pivotPoint int) []int {
+    if pivotPoint > 0 {
+        left := arr[0:pivotPoint]
+        right := arr[pivotPoint:]
+
+        left = mergesortActual(left, pivotPointFn(left))
+        right = mergesortActual(right, pivotPointFn(right))
+
+        arr = merge(left, right)
+    } else {
+        return arr
+    }
+
+    return arr
+}
+
+func merge(left, right []int) []int {
+    var newArr []int
+
+    leftIndex := 0
+    rightIndex := 0
+
+    for leftIndex < len(left) || rightIndex < len(right) {
+        if leftIndex >= len(left) {
+            newArr = append(newArr, right[rightIndex])
+            rightIndex++
+        } else if rightIndex >= len(right) {
+            newArr = append(newArr, left[leftIndex])
+            leftIndex++
+        } else if left[leftIndex] < right[rightIndex] {
+            newArr = append(newArr, left[leftIndex])
+            leftIndex++
+        } else {
+            newArr = append(newArr, right[rightIndex])
+            rightIndex++
+        }
+    }
+
+    fmt.Println(leftIndex, left, rightIndex, right, newArr)
+
+    return newArr
 }
