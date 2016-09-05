b5195d36c414bb4c42ba992a083ede08c456a775
Jean de Klerk <jadekler@gmail.com>
2016-09-05 10:30:15 -0600

    Bubblesort done

--- sort/bubblesort.go
+++ sort/bubblesort.go
@@ -1,5 +1,21 @@
 package sort
 
 func Bubblesort(arr []int) []int {
-    return []int{}
+    var swapped bool
+    var hold int
+
+    for i := 0; i < len(arr) - 1; i++ {
+        if arr[i] > arr[i+1] {
+            swapped = true
+            hold = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = hold
+        }
+    }
+
+    if swapped {
+        return Bubblesort(arr)
+    }
+
+    return arr
 }
--- sort/bubblesort_test.go
+++ sort/bubblesort_test.go
@@ -6,9 +6,27 @@ import (
     "reflect"
 )
 
-func TestBubblesort(t *testing.T) {
+func TestBubblesortSimple(t *testing.T) {
     actual := sort.Bubblesort([]int{3, 1, 2})
     expected := []int{1, 2, 3}
+    if !reflect.DeepEqual(actual, expected) {
+        t.Logf("Expected %v, got %v", expected, actual)
+        t.Fail()
+    }
+}
+
+func TestBubblesortSame(t *testing.T) {
+    actual := sort.Bubblesort([]int{1, 2, 3})
+    expected := []int{1, 2, 3}
+    if !reflect.DeepEqual(actual, expected) {
+        t.Logf("Expected %v, got %v", expected, actual)
+        t.Fail()
+    }
+}
+
+func TestBubblesortComplex(t *testing.T) {
+    actual := sort.Bubblesort([]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1})
+    expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
     if !reflect.DeepEqual(actual, expected) {
         t.Logf("Expected %v, got %v", expected, actual)
         t.Fail()
