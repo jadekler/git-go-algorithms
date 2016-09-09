137e2d3c008a860ed661987a2bc13fb085863ab2
Jean de Klerk <jadekler@gmail.com>
2016-09-08 20:01:04 -0600

    Begin attempt on spiral problem

--- problems/spiral_print.go
+++ problems/spiral_print.go
@@ -1,5 +1,18 @@
 package problems
 
 func SpiralPrint(arr [][]int) []int {
-    return []int{}
+    var res []int
+
+    width := len(arr[0])
+    height := len(arr)
+    totalElems := width * height
+
+    for i := 0; i < totalElems; i++ {
+        row := i / width
+        column := i - (row * width)
+
+        res = append(res, arr[row][column])
+    }
+
+    return res
 }
